b556cce0e7a9bc57932e17c16976a0a85b41a960
Buff99 <buffbuffster@gmail.com>
2017-05-22 02:47:37 +0100

    bye-bye gello

--- jf-common.mk
+++ jf-common.mk
@@ -109,9 +109,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:/system/etc/gps.conf \
     $(LOCAL_PATH)/configs/sap.conf:/system/etc/sap.conf
 
-PRODUCT_PACKAGES += \
-    Gello
-
 # IPv6 tethering
 PRODUCT_PACKAGES += \
     ebtables \
