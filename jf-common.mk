c632f3a0cea115140539b0b14abb1e895f6eee81
Buff99 <buffbuffster@gmail.com>
2017-06-27 10:46:21 +0100

    Qcom: add voiceproc

--- jf-common.mk
+++ jf-common.mk
@@ -118,6 +118,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     consumerir.msm8960
 
+# Voice processing 
+PRODUCT_PACKAGES += \
+    libqcomvoiceprocessing
+    
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/atmel_mxt_ts.kl:system/usr/keylayout/atmel_mxt_ts.kl \
