c81cf542b3ea3361d917289b0fbefe4e9f8a82a6
Buff99 <buffbuffster@gmail.com>
2017-08-08 02:42:30 +0100

    Thermal: try reverting some older config

--- jf-common.mk
+++ jf-common.mk
@@ -198,6 +198,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf \
     $(LOCAL_PATH)/configs/thermal-engine-8064ab.conf:system/etc/thermal-engine-8064ab.conf
 
 # Wifi
